e1ac999904023e3a2ae1cba6d76bc9094fea6758
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-04 23:18:49 +0100

    - use strong type

--- NeoGeoSolver.NET/Constraints/PointsCoincident.cs
+++ NeoGeoSolver.NET/Constraints/PointsCoincident.cs
@@ -4,13 +4,13 @@ using NeoGeoSolver.NET.Solver;
 namespace NeoGeoSolver.NET.Constraints;
 
 public class PointsCoincident : Constraint {
-	public IEntity p0 { get { return GetEntity(0); } set { SetEntity(0, value); } }
-	public IEntity p1 { get { return GetEntity(1); } set { SetEntity(1, value); } }
+	public Point p0 { get { return (Point) GetEntity(0); } set { SetEntity(0, value); } }
+	public Point p1 { get { return (Point) GetEntity(1); } set { SetEntity(1, value); } }
 
 	public override IEnumerable<Expression> equations {
 		get {
-			var pe0 = p0.GetPointAtInPlane(0);
-			var pe1 = p1.GetPointAtInPlane(0);
+			var pe0 = p0.exp;
+			var pe1 = p1.exp;
 			yield return pe0.x - pe1.x;
 			yield return pe0.y - pe1.y;
 		}
