3c3d1f52f662e52ee20d17c63ee75721dc282674
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-05 19:07:46 +0100

    - code cleanup

--- NeoGeoSolver.NET/Entities/Ellipse.cs
+++ NeoGeoSolver.NET/Entities/Ellipse.cs
@@ -2,39 +2,53 @@
 
 namespace NeoGeoSolver.NET.Entities;
 
-public class Ellipse : Entity {
+public class Ellipse : Entity
+{
+  public readonly Point Centre = new();
+  public readonly Param Radius0 = new("r0");
+  public readonly Param Radius1 = new("r1");
+  private readonly ExpressionBasis2d _basis = new();
 
-	public Point c = new();
-	public Param r0 = new("r0");
-	public Param r1 = new("r1");
-	private ExpressionBasis2d _basis = new();
+  public Ellipse(Point centre, Param rad0, Param rad1)
+  {
+    Centre = centre;
+    Radius0 = rad0;
+    Radius1 = rad1;
+    _basis.SetPosParams(Centre.x, Centre.y);
+  }
 
-	public Ellipse()
-	{
-		_basis.SetPosParams(c.x, c.y);
-	}
+  public override IEnumerable<Param> Parameters
+  {
+    get
+    {
+      yield return Radius0;
+      yield return Radius1;
+      foreach (var p in _basis.parameters)
+      {
+        yield return p;
+      }
+    }
+  }
 
-	public override IEnumerable<Param> Parameters {
-		get {
-			yield return r0;
-			yield return r1;
-			foreach(var p in _basis.parameters) yield return p;
-		}
-	}
+  public override IEnumerable<Expression> Equations
+  {
+    get
+    {
+      foreach (var e in _basis.equations)
+      {
+        yield return e;
+      }
+    }
+  }
 
-	public override IEnumerable<Expression> Equations {
-		get {
-			foreach(var e in _basis.equations) yield return e;
-		}
-	}
+  public ExpressionVector CentreExpr()
+  {
+    return Centre.exp;
+  }
 
-	public ExpressionVector Center()
-	{
-		return c.exp;
-	}
-
-	public override ExpressionVector PointOn(Expression t) {
-		var angle = t * 2.0 * Math.PI;
-		return _basis.TransformPosition(new ExpressionVector(Expression.Cos(angle) * Expression.Abs(r0), Expression.Sin(angle) * Expression.Abs(r1), 0.0));
-	}
+  public override ExpressionVector PointOn(Expression t)
+  {
+    var angle = t * 2.0 * Math.PI;
+    return _basis.TransformPosition(new ExpressionVector(Expression.Cos(angle) * Expression.Abs(Radius0), Expression.Sin(angle) * Expression.Abs(Radius1), 0.0));
+  }
 }
