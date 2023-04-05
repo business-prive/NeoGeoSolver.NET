6709bf802465f2a382ef25b296d5ca6142cdedb3
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-05 17:51:09 +0100

    - code cleanup

--- NeoGeoSolver.NET/Constraints/PointLineDistance.cs
+++ NeoGeoSolver.NET/Constraints/PointLineDistance.cs
@@ -5,53 +5,23 @@ namespace NeoGeoSolver.NET.Constraints;
 
 public class PointLineDistance : Value
 {
-	public Point point
-	{
-		get;set;
-	}
+  public Point Point { get; }
 
-	public Line line
-	{
-		get;set;
-	}
+  public Line Line { get; }
 
-	public ExpressionVector pointExp
-	{
-		get
-		{
-			return point.exp;
-		}
-	}
+  public PointLineDistance(Point pt, Line line)
+  {
+    Point = pt;
+    Line = line;
+    SetValue(1.0);
+    Satisfy();
+  }
 
-	public ExpressionVector lineP0Exp
-	{
-		get
-		{
-			return line.p0.exp;
-		}
-	}
-
-	public ExpressionVector lineP1Exp
-	{
-		get
-		{
-			return line.p1.exp;
-		}
-	}
-
-	public PointLineDistance(Point pt, Line line)
-	{
-		point = pt;
-		this.line = line;
-		SetValue(1.0);
-		Satisfy();
-	}
-
-	public override IEnumerable<Expression> equations
-	{
-		get
-		{
-			yield return ConstraintExp.PointLineDistance(pointExp, lineP0Exp, lineP1Exp) - value;
-		}
-	}
+  public override IEnumerable<Expression> equations
+  {
+    get
+    {
+      yield return ConstraintExp.PointLineDistance(Point.exp, Line.p0.exp, Line.p1.exp) - value;
+    }
+  }
 }
