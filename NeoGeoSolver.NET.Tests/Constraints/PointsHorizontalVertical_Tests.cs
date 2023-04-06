38859c62967c5b5db1dfc0e531ea4f5490866550
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-06 13:48:48 +0100

    - horiz/vert point constraint tests

---
+++ NeoGeoSolver.NET.Tests/Constraints/PointsHorizontalVertical_Tests.cs
@@ -0,0 +1,39 @@
+using NeoGeoSolver.NET.Utils;
+
+namespace NeoGeoSolver.NET.Tests.Constraints;
+
+[TestFixture]
+public sealed class PointsHorizontalVertical_Tests
+{
+  [Test]
+  public void Vertical_works()
+  {
+    var pt0 = new Point(0, 0, 0);
+    var pt1 = new Point(10, 10, 0);
+    var constr = new PointsHorizontalVertical(pt0, pt1, HorizontalVerticalOrientation.Ox);
+    var eqnSys = new EquationSystem();
+    eqnSys.AddEquations(constr.Equations);
+    eqnSys.AddParameter(pt1.x);
+
+    var result = eqnSys.Solve();
+
+    result.Should().Be(EquationSystem.SolveResult.Okay);
+    pt1.x.Value.Should().Be(0);
+  }
+
+  [Test]
+  public void Horizontal_works()
+  {
+    var pt0 = new Point(0, 0, 0);
+    var pt1 = new Point(10, 10, 0);
+    var constr = new PointsHorizontalVertical(pt0, pt1, HorizontalVerticalOrientation.Oy);
+    var eqnSys = new EquationSystem();
+    eqnSys.AddEquations(constr.Equations);
+    eqnSys.AddParameter(pt1.y);
+
+    var result = eqnSys.Solve();
+
+    result.Should().Be(EquationSystem.SolveResult.Okay);
+    pt1.y.Value.Should().Be(0);
+  }
+}
--- NeoGeoSolver.NET.Tests/Usings.cs
+++ NeoGeoSolver.NET.Tests/Usings.cs
@@ -1,3 +1,6 @@
 global using NUnit.Framework;
 global using FluentAssertions;
 global using FluentAssertions.Execution;
+global using NeoGeoSolver.NET.Entities;
+global using NeoGeoSolver.NET.Constraints;
+global using NeoGeoSolver.NET.Solver;
