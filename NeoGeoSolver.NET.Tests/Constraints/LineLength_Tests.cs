37d8e11e2b7ad64757e31f06a6a32a791c8bba71
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-06 21:48:10 +0100

    - line length constraint test

---
+++ NeoGeoSolver.NET.Tests/Constraints/LineLength_Tests.cs
@@ -0,0 +1,26 @@
+namespace NeoGeoSolver.NET.Tests.Constraints;
+
+[TestFixture]
+public sealed class LineLength_Tests
+{
+  [Test]
+  public void Length_works()
+  {
+    var pt0 = new Point(0, 0, 0);
+    var pt1 = new Point(10, 10, 0);
+    var line = new Line(pt0, pt1);
+    var constr = new LineLength(line);
+    constr.SetValue(10);
+    var eqnSys = new EquationSystem();
+    eqnSys.AddEquations(constr.Equations);
+    eqnSys.AddParameter(line.Point1.x);
+
+    var result = eqnSys.Solve();
+
+    using (new AssertionScope())
+    {
+      result.Should().Be(EquationSystem.SolveResult.Okay);
+      line.Point1.x.Value.Should().BeApproximately(0, 1e-4);
+    }
+  }
+}
