d0872779b04c781d3a494416fc845cddaa546268
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-06 10:45:45 +0100

    - moved to file to show what is being tested

--- NeoGeoSolver.NET.Tests/Utils/ExpressionParser_Tests.cs
+++ NeoGeoSolver.NET.Tests/Utils/ExpressionParser_Tests.cs
@@ -28,28 +28,4 @@ public sealed class ExpressionParser_Tests
 
     act.Should().NotThrow();
   }
-
-  [TestCase("2 * 3", 6.0)]
-  [TestCase("2 + 1", 3.0)]
-  [TestCase("-2 + 2", 0.0)]
-  [TestCase("+2 - -2", 4.0)]
-  [TestCase("-2 * -2", 4.0)]
-  [TestCase("+1 * +2", 2.0)]
-  [TestCase("2 + 3 * 6", 20.0)]
-  [TestCase("2 + (3 * 6)", 20.0)]
-  [TestCase("(2 + 3) * 6", 30.0)]
-  [TestCase("((2 + 3) * (6))", 30.0)]
-  [TestCase("cos(0)", 1.0)]
-  [TestCase("sqr(cos(2)) + sqr(sin(2))", 1.0)]
-  [TestCase("pi", Math.PI)]
-  [TestCase("e", Math.E)]
-  public static void Eval_returns_expected(string exprStr, double expResult)
-  {
-    var parser = new ExpressionParser(exprStr);
-    var exp = parser.Parse();
-
-    var result = exp.Eval();
-
-    result.Should().Be(expResult);
-  }
 }
