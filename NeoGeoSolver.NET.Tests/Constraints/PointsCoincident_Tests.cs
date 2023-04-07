c19acde44ad0bc817ef03705dbb3650d9d54e657
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-07 15:49:58 +0100

    - points coincident constraint test

---
+++ NeoGeoSolver.NET.Tests/Constraints/PointsCoincident_Tests.cs
@@ -0,0 +1,27 @@
+namespace NeoGeoSolver.NET.Tests.Constraints;
+
+[TestFixture]
+public class PointsCoincident_Tests
+{
+  [Test]
+  public void PointsCoincident_work()
+  {
+    var p0 = new Point(0, 0, 0);
+    var p1 = new Point(5, 10, 0);
+    var constr = new PointsCoincident(p0, p1);
+    var eqnSys = new EquationSystem();
+    eqnSys.AddEquations(constr.Equations);
+    eqnSys.AddParameter(p1.x);
+    eqnSys.AddParameter(p1.y);
+
+    var result = eqnSys.Solve();
+
+    using (new AssertionScope())
+    {
+      result.Should().Be(EquationSystem.SolveResult.Okay);
+      p1.x.Value.Should().BeApproximately(0, 1e-6);
+      p1.y.Value.Should().BeApproximately(0, 1e-6);
+    }
+  }
+  
+}
