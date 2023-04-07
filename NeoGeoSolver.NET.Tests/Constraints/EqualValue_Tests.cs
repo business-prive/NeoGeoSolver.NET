f143320bf0ccc629281011b4595b2c86415554c9
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-04-07 22:27:56 +0100

    - equal value constraint test

---
+++ NeoGeoSolver.NET.Tests/Constraints/EqualValue_Tests.cs
@@ -0,0 +1,33 @@
+namespace NeoGeoSolver.NET.Tests.Constraints;
+
+[TestFixture]
+public sealed class EqualValue_Tests
+{
+  [Test]
+  public void Equal_works()
+  {
+    var val0 = new DummyValue(10);
+    var val1 = new DummyValue(0);
+    var constr = new EqualValue(val0, val1);
+    constr.SetValue(5);
+    var eqnSys = new EquationSystem();
+    eqnSys.AddEquations(constr.Equations);
+    eqnSys.AddParameter(val1.GetValueParam());
+
+    var result = eqnSys.Solve();
+
+    using (new AssertionScope())
+    {
+      result.Should().Be(EquationSystem.SolveResult.Okay);
+      val1.GetValue().Should().BeApproximately(5, 1e-6);
+    }
+  }
+  
+  private sealed class DummyValue : Value
+  {
+    public DummyValue(double val)
+    {
+      SetValue(val);
+    }
+  }
+}
